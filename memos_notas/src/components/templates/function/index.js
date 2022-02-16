ba8996908bdb2f242cbab32e914d1d56a1d63c59
Ramiro Centeno <rdavid3.1991@gmail.com>
2022-02-16 13:09:55 -0500

    Fix 🔧: Refactirizacion de funcion

--- memos_notas/src/components/templates/function/index.js
+++ memos_notas/src/components/templates/function/index.js
@@ -1,11 +1,1 @@
-import { apiRequest } from "src/utils/apiRequest"
 
-/**
- * 
- * @param {Object} params 
- */
-export const fetchTemplate = async () => {
-   const response = await apiRequest().get("get_template_doc", {})
-
-   console.log(response);
-}
